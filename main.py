aa7bcca44f36136624f35af1796f6c88d1ada605
коротков <korotkov111@icloud.com>
2021-03-24 11:14:33 +0700

    изменение сайта 3

--- main.py
+++ main.py
@@ -1,43 +1,15 @@
-from flask import Flask, url_for
+from flask import Flask, render_template
+from werkzeug.utils import redirect
+
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-def index():
-    return """"""
-
-@app.route('/index')
-def countdown():
-    countdown_list = []
-    countdown_list.append('И на Марсе будут яблони цвести!')
-    return '</br>'.join(countdown_list)
-
-@app.route('/promotion')
-def promotion():
-    countdown_list = []
-    countdown_list.append('Человечество вырастает из детства.')
-    countdown_list.append('Человечеству мала одна планета.')
-    countdown_list.append('Мы сделаем обитаемыми безжизненные пока планеты.')
-    countdown_list.append('И начнем с Марса!')
-    countdown_list.append('Присоединяйся!')
-    return '</br>'.join(countdown_list)
-
-
-@app.route('/image_mars')
-def return_sample_page():
-    return """<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="/static/img/mars.jpg" alt="здесь должна была быть картинка, но не нашлась">
-                    <h6>Вот она красная планета!</h6>
-                  </body>
-                </html>"""
+def login():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
